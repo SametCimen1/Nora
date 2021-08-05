48fd2c85feaef668d6cf3d05c3fec81a2c735523
SametCimen1 <cimensamet338@gmail.com>
2021-08-04 20:10:58 -0400

    added client

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,42 @@
+import {useState} from 'react';
 const Register = () =>{
+    const [information, setInformation] = useState({name:'', firstName:'', lastName:'', email:'', password:''});
+
+    const submitForm = async(e) =>{
+        e.preventDefault();
+        if(information.name !== '' && information.firstName !== ''&& information.lastName !== ''&& information.email !== '' && information.password !== ''){
+           const data = await fetch(`http://localhost:5000/register`, {
+               method:"POST",
+               headers:{"Content-Type":"application/json"},
+               body: {
+                  name: information.name,
+                  password: information.password,
+                  first_name:information.firstName,
+                  last_name:information.lastName,
+                  email: information.email
+               }
+           })
+           console.log(data)
+        }
+        else{
+            alert("fill the fields")
+        }
+      }
+
+
     return (
-        <h1>Register Page</h1>
+        <div style = {{width:"20%", marginTop:"10rem", display:'flex' ,height:500, justifyContent:'center', alignItems:'center', margin:"auto"}}>
+            <form onSubmit = {submitForm}>
+                <input type = "text" className = "form-control" placeholder = "name" onChange = {(e) => setInformation(prev => ({...prev, name:e.target.value} ))} value = {information.name}  />
+                <input type = "text" className = "form-control mt-5" placeholder = "first name"  onChange = {(e) => setInformation(prev => ({...prev, firstName:e.target.value}))} value = {information.firstName}  /> 
+                <input type = "text" className = "form-control mt-5" placeholder = "last name" onChange = {(e) => setInformation(prev => ({...prev, lastName:e.target.value}))} value = {information.lastName}  />
+                <input type = "email" className = "form-control mt-5" placeholder = "email"  onChange = {(e) => setInformation(prev => ({...prev, email:e.target.value}))} value = {information.email}  />
+                <input type = "password" className = "form-control mt-5" placeholder = "password"  onChange = {(e) => setInformation(prev => ({...prev,password:e.target.value}))} value = {information.password}  />                   
+                <button type = "submit" className = "btn bg-primary w-100 mt-5" >Login</button>
+            </form>
+    </div>
     )
 }
 
 export default Register;
+
--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
 const express  = require('express');
-const app = express();
 require('dotenv').config();
-app.use(express.json());
 const bcrypt = require('bcrypt');
 const pool = require('./Pool');
 var jwt = require('jsonwebtoken');
-const auth = require('./middleware/auth');
-const cookieParser = require('cookie-parser');
+// const auth = require('./middleware/auth');
+// const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-app.use(cors());
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(cors({credentials: true, origin: true}));
+var jsonParser = bodyParser.json()
+app.use(express.json());
+// parse application/json
+
+
 // app.use(cors({origin:'http://localhost:3000', credentials: true}))
-app.use(cookieParser());
-app.disable('x-powered-by');
-app.use(function(req, res) {
-    // Website you wish to allow to connect
- 
-})
+// app.use(cookieParser());
+
 
 app.get('/', (req,res) =>{
     res.json(req.cookies);
@@ -28,36 +30,37 @@ app.get('/welcome', (req,res)=>{
 })
 
 
-app.post("/register", async(req,res) =>{
+app.post("/register",jsonParser, async(req,res) =>{
+    // try {
+    //     console.log(req.body)
+    //     const {name, first_name, last_name, email, password} = req.body
+    //     if (!(email && password && first_name && last_name)) {
+    //         res.status(400).send("All input is required");
+    //     }
+    //     //CHECK IF USER ALREADY EXIST;
 
-    try {
-        const {name,first_name, last_name, email, password} = req.body
-        if (!(email && password && first_name && last_name)) {
-            res.status(400).send("All input is required");
-        }
-        //CHECK IF USER ALREADY EXIST;
-
-        //ENCRYPT PASSWORD
-        const encryptedPassword = await bcrypt.hash(password, 10);
+    //     //ENCRYPT PASSWORD
+    //     const encryptedPassword = await bcrypt.hash(password, 10);
         
-        //CREATE USER IN DB WITH THE encryptesPassword 
-         const today = new Date().toISOString().split('T')[0];
-        console.log(today)
+    //     //CREATE USER IN DB WITH THE encryptesPassword 
+    //      const today = new Date().toISOString().split('T')[0];
+    //     console.log(today)
         
-         const user = await pool.query(`INSERT INTO users (name, first_name, last_name, email, password, joined, words,streak) VALUES ('${name}', '${first_name}', '${last_name}', '${email}', '${encryptedPassword}',' ${today}', 0, 0) RETURNING *`);
-         console.log(user.rows[0])
-        // CREATE TOKEN
-        const token = jwt.sign({
-            data: {userId:user.id, userEmail: user.email}
-          }, 'secret', { expiresIn: '1h' });
+    //      const user = await pool.query(`INSERT INTO users (name, first_name, last_name, email, password, joined, words,streak) VALUES ('${name}', '${first_name}', '${last_name}', '${email}', '${encryptedPassword}',' ${today}', 0, 0) RETURNING *`);
+    //      console.log(user.rows[0])
+    //     // CREATE TOKEN
+    //     const token = jwt.sign({
+    //         data: {userId:user.id, userEmail: user.email}
+    //       }, 'secret', { expiresIn: '1h' });
 
-        user.token = token;
-        res.cookie('token', token, {maxAge: 60*60*24*7, httpOnly: true})
-        res.status(201).json(user)
+    //     user.token = token;
+    //     res.cookie('token', token, {maxAge: 60*60*24*7, httpOnly: true})
+    //     res.status(201).json(user)
 
-    } catch (error) {
-        console.log(error)
-    }
+    // } catch (error) {
+    //     console.log(error)
+    // }
+    console.log(req.body)
 })
 
 app.post("/login", async(req,res) =>{
